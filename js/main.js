ffa80ebcae98d31039b928b09811effa5d43bea7
Gisela Rios <gisee_rios@hotmail.com>
2022-07-11 21:29:52 -0300

    simulador interactivo

--- js/main.js
+++ js/main.js
@@ -1,27 +1,56 @@
 
-/*----ASIGNACION DE TURNOS----*/
+//------------------------------------//
+//---------Agregar al carrito---------//
+//------------------------------------//
 
-for (let i = 1; i <= 10; i++) {
-    let ingresarNombre = prompt("Ingrese su nombre");
-    let peso = prompt("Ingrese su peso")
-    let altura = prompt("Ingrese su altura en metros.");
-    const resultado = peso / altura ** 2;
-    /*----- CALCULADORA IMC------*/
-    if (i <= 10) {
-        if (resultado < 18.5) {
-            alert(ingresarNombre + " estas por debajo de tu peso ideal... consulta con nuestro nutricionista.")
-        }
-        else if ((resultado >= 18.5) && (resultado <= 24.9)) {
-            alert(ingresarNombre + " tenes un peso saludable... consulta a nuestro nutricionista para seguir asi!")
-        }
-        else if ((resultado >= 25) && (resultado <= 29.9)) {
-            alert(ingresarNombre + " tenes sobrepeso...saca un turno con nuestros especialistas.")
-        }
-        else {
-            alert(ingresarNombre + " tenes obesidad...saca un turno con nuestros especialistas.")
-        }
-        alert("Turno N " + i);
-        
-    }    
+function agregarCarrito(producto, color, precio, stock){          
+    const tenemosStock = stockReal(stock);
+    if(tenemosStock == 'tenemos stock'){
+        console.log('Producto '+ producto +' fue agregado a tu carrito en color '+ color + ' cuesta $'+ precio + ' por unidad');
+    }else{
+        console.log('No tenemos stock de: '+ producto);
+    }  
 }
-alert("Te pedimos disculpas no tenemos mas turno.")
+console.log('Tenga en cuenta que el tiempo de fabricacion son 35 dias')
+
+//------------------------------------//
+//----------------Stock---------------//
+//------------------------------------//
+
+function stockReal(stock){
+    if(stock >0){
+        return 'tenemos stock';
+    }else{
+        return ' Te pedimos disculpas no tenemos stock'
+    }
+}
+
+//------------------------------------//
+//-Contador de cliente para promocion-//
+//------------------------------------//
+for (let i = 1; i <= 3; i++) {
+    let ingresarNombre = prompt('Ingresa tu nombre');
+    let mail = prompt('Bienvenida/o ' + ingresarNombre + ' ingresa tu mail y recibi las ofertas del mes');
+    
+    //----Llamamos la funcion------------//
+    agregarCarrito('sillon', 'blanco', 18000, 1);
+    agregarCarrito('mesa', 'gris', 25000, 0);
+    agregarCarrito('silla', 'rojo', 4000, 1);
+    agregarCarrito('sillon', 'negro', 18000, 2)
+
+    if(i == 3){
+        alert('Felicitaciones eres nuestro cliente numero 3 y te regalamos un %10 off')
+    }
+    
+}    
+
+
+
+
+
+
+
+
+
+
+
