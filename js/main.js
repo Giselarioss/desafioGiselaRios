645cd5b33398f0f1b383b400aeec2089a9623e00
Gisela Rios <gisee_rios@hotmail.com>
2022-07-12 14:52:54 -0300

    corecciones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+//------------------------------------//
+//-Contador de cliente para promocion-//
+//------------------------------------//
+
+for (let i = 1; i <= 5; i++) {
+    let ingresarNombre = prompt('Ingresa tu nombre');
+    let ingreasarMail = prompt('Bienvenida/o ' + ingresarNombre + ' ingresa tu mail y recibi las ofertas del mes');
+    if(i == 3){
+        console.log('Felicitaciones eres nuestro cliente numero 3 y te regalamos un %10 off')
+    }
+    else if( i == 5){
+        console.log('Eres el ultimo pedido del dia y el tiempo de entrega excede las 24 hs') 
+    }    
+}  
 
 //------------------------------------//
 //---------Agregar al carrito---------//
@@ -8,7 +22,7 @@ function agregarCarrito(producto, color, precio, stock){
     if(tenemosStock == 'tenemos stock'){
         console.log('Producto '+ producto +' fue agregado a tu carrito en color '+ color + ' cuesta $'+ precio + ' por unidad');
     }else{
-        console.log('No tenemos stock de: '+ producto);
+        console.log('No tenemos stock de: '+ producto +', en una semana vuelve a entrar');
     }  
 }
 
@@ -25,35 +39,8 @@ function stockReal(stock){
     }
 }
 
-//------------------------------------//
-//-Contador de cliente para promocion-//
-//------------------------------------//
-for (let i = 1; i <= 5; i++) {
-    let ingresarNombre = prompt('Ingresa tu nombre');
-    let mail = prompt('Bienvenida/o ' + ingresarNombre + ' ingresa tu mail y recibi las ofertas del mes');
-    
-    //----Llamamos la funcion------------//
-    agregarCarrito('sillon', 'blanco', 18000, 1);
-    agregarCarrito('mesa', 'gris', 25000, 0);
-    agregarCarrito('silla', 'rojo', 4000, 1);
-    agregarCarrito('sillon', 'negro', 18000, 2)
-
-    if(i == 3){
-        console.log('Felicitaciones eres nuestro cliente numero 3 y te regalamos un %10 off')
-    }
-    else if( i == 5){
-        console.log('Eres el ultimo pedido del dia y el tiempo de entrega excede las 24 hs') 
-    }
-    
-}    
-
-
-
-
-
-
-
-
-
-
-
+//----Llamamos la funcion------------//
+agregarCarrito('sillon', 'blanco', 18000, 1);
+agregarCarrito('mesa', 'gris', 25000, 0);
+agregarCarrito('silla', 'rojo', 4000, 1);
+agregarCarrito('sillon', 'negro', 18000, 2)
