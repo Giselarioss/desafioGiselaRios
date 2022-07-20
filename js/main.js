ec7090fef6697a6da129ee7e82d30033d2fcdb2e
Gisela Rios <gisee_rios@hotmail.com>
2022-07-19 21:16:17 -0300

    array

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
+let ingresarNombre = prompt('Ingresa tu nombre');
+let ingreasarMail = prompt('Bienvenida/o ' + ingresarNombre + ' ingresa tu mail y recibi las ofertas del mes');
+
 //------------------------------------//
-//---------Agregar al carrito---------//
+//--------------productos-------------//
 //------------------------------------//
 
-function agregarCarrito(producto, color, precio, stock){          
-    const tenemosStock = stockReal(stock);
-    if(tenemosStock == 'tenemos stock'){
-        console.log('Producto '+ producto +' fue agregado a tu carrito en color '+ color + ' cuesta $'+ precio + ' por unidad');
-    }else{
-        console.log('No tenemos stock de: '+ producto +', en una semana vuelve a entrar');
-    }  
-}
+const carrito = [
+    { id:001, name: 'sillon', precio: 18000},
+    { id:002,name: 'silla', precio: 4000},
+    { id:003, name: 'mesa', precio: 25000},     
+]
 
 
 //------------------------------------//
@@ -20,31 +20,51 @@ function stockReal(stock){
     if(stock >0){
         return 'tenemos stock';
     }else{
-        return ' Te pedimos disculpas no tenemos stock'
+        return ' Te pedimos disculpas no tenemos stock';
     }
 }
 
 
+
 //------------------------------------//
-//-Contador de cliente para promocion-//
+//---------Agregar al carrito---------//
 //------------------------------------//
 
-for (let i = 1; i <= 5; i++) {
-    let ingresarNombre = prompt('Ingresa tu nombre');
-    let ingreasarMail = prompt('Bienvenida/o ' + ingresarNombre + ' ingresa tu mail y recibi las ofertas del mes');
+function agregarCarrito(producto, stock, color){                     
+    const tenemosStock = stockReal(stock); 
+    if(tenemosStock == 'tenemos stock'){        
+        console.log(producto +' fue agregado a tu carrito de compras, en color  '+ color + '.');
+    }else{ 
+        if(stock == 0){            
+            console.log('No tenemos stock de: '+ producto +', en una semana vuelve a entrar');
 
-    //---------Llamamos la funcion------------//
-    agregarCarrito('sillon', 'blanco', 18000, 1);
-    agregarCarrito('mesa', 'gris', 25000, 0);
-    agregarCarrito('silla', 'rojo', 4000, 1); 
+        }       
+        
+    }       
+}
 
-    if(i == 3){
-        alert('Felicitaciones eres nuestro cliente numero 3 y te regalamos un %10 off')
-    }
-    else if( i == 5){
-        alert('Eres el ultimo pedido del dia y el tiempo de entrega excede las 24 hs') 
-    }         
-}  
+//------------------------------------//
+//------Borrar producto carrito-------//
+//------------------------------------//
+
+function borrarProductoCarrito (idProducto){
+    const index = carrito.findIndex((producto) => producto.id === idProducto); 
+    carrito.splice(index,1)   
+    console.log(carrito);  
+}
+
+
+//---------Llamamos la funcion para agregar al carrito------------//
+agregarCarrito('sillon', 1, 'rosa');
+agregarCarrito('mesa', 1,'negro');
+agregarCarrito('silla', 1,'rojo');  
+
+
+//---------Llamamos la funcion para borrar al carrito------------//
+borrarProductoCarrito(001); 
+
+const total = (carrito.reduce((acc,el)=>acc+el.precio,0));
+console.log('Total a pagar $'+ total);
 
 
 
