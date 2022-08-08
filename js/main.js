d8988a3f175b596c70323b876a63316a4f6b2b1e
Gisela Rios <gisee_rios@hotmail.com>
2022-08-08 07:12:13 -0300

    probando

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
 //---- ?? pregunta si es undefined o null devuelve array vacio, si no devuelve el valor que esta ahi
 const miCarrito = JSON.parse(localStorage.getItem('miCarrito')) ?? [];
 const total = miCarrito.reduce((acc, el) => acc + el.precio, 0);
+
 //---- modifica el numero en el carrito
 document.getElementById('mi-compra').innerHTML = `${miCarrito.length}`;
 document.getElementById('total-compra').innerHTML = `$${total}`;
+
+
+
 
 
 
@@ -14,42 +31,42 @@ document.getElementById('total-compra').innerHTML = `$${total}`;
 //------------------------------------//
 
 const carrito = [
-    { id:001, producto: 'Sillon Mand', precio: 80000, img:'imagenes/sillon/sillon.png', categoria: 'Sillon' },
-    { id:002, producto: 'Sillon Candy', precio: 60000, img:'imagenes/sillon/sillon2.png', categoria: 'Sillon' },
-    { id:003, producto: 'Sillon Matt', precio: 47000, img:'imagenes/sillon/sillon3.png', categoria: 'Sillon' },
-    { id:004, producto: 'Sillon Sindy', precio: 80000, img:'imagenes/sillon/sillon4.png', categoria: 'Sillon' },
-    { id:005, producto: 'Sillon Ted', precio: 90000, img:'imagenes/sillon/sillon5.png', categoria: 'Sillon' },
-    { id:006, producto: 'Sillon Thuk', precio: 70000, img:'imagenes/sillon/sillon6.png', categoria: 'Sillon' },
-    { id:007, producto: 'Sillon Grace', precio: 80000, img:'imagenes/sillon/sillon8.png', categoria: 'Sillon' },
-    { id:010, producto: 'Sillon Furt', precio: 90000, img:'imagenes/sillon/sillon9.png', categoria: 'Sillon' },
-    { id:011, producto: 'Sillon Huk', precio: 75000, img:'imagenes/sillon/sillon10.png', categoria: 'Sillon' },
-    { id:012, producto: 'Sillon Morf', precio: 87000, img:'imagenes/sillon/sillon11.png', categoria: 'Sillon' },
-    { id:013, producto: 'Sillon Mali', precio: 76000, img:'imagenes/sillon/sillon12.png', categoria: 'Sillon' },
-    { id:014, producto: 'Sillon Lou', precio: 80000, img:'imagenes/sillon/sillon15.png', categoria: 'Sillon' },
-    { id:015, producto: 'Silla Yug', precio: 79000, img:'imagenes/silla/silla.png', categoria: 'Silla' },
-    { id:016, producto: 'Silla Wind', precio: 64000, img:'imagenes/silla/silla1.png', categoria: 'Silla' },
-    { id:017, producto: 'Silla Meli', precio: 83000, img:'imagenes/silla/silla2.png', categoria: 'Silla' },
-    { id:020, producto: 'Silla Frut', precio: 35000, img:'imagenes/silla/silla3.png', categoria: 'Silla' },
-    { id:021, producto: 'Silla Xyl', precio: 47000, img:'imagenes/silla/silla4.png', categoria: 'Silla' },
-    { id:022, producto: 'Silla Grey', precio: 48000, img:'imagenes/silla/silla5.png', categoria: 'Silla' },
-    { id:023, producto: 'Mesa Sam', precio: 99000, img:'imagenes/mesa/mesa1.png', categoria: 'Mesa' },
-    { id:024, producto: 'Mesa Matt', precio: 89000, img:'imagenes/mesa/mesa2.png', categoria: 'Mesa' },
-    { id:025, producto: 'Mesa Saum', precio: 99000, img:'imagenes/mesa/mesa3.png', categoria: 'Mesa' },
-    { id:026, producto: 'Mesa Dot', precio: 89000, img:'imagenes/mesa/mesa4.png', categoria: 'Mesa' },
-    { id:027, producto: 'Mesa Frut', precio: 79000, img:'imagenes/mesa/mesa5.png', categoria: 'Mesa' },
-    { id:030, producto: 'Mesa Saim', precio: 69000, img:'imagenes/mesa/mesa6.png', categoria: 'Mesa' },
-    { id:031, producto: 'Mesa Ros', precio: 89000, img:'imagenes/mesa/mesa7.png', categoria: 'Mesa' },
-    { id:032, producto: 'Mesa Will', precio: 87000, img:'imagenes/mesa/mesa8.png', categoria: 'Mesa' },
-    { id:033, producto: 'Mesa Ann', precio: 95000, img:'imagenes/mesa/mesa9.png', categoria: 'Mesa' },
-    { id:034, producto: 'Mesa Drey', precio: 93000, img:'imagenes/mesa/mesa10.png', categoria: 'Mesa' },
-    { id:035, producto: 'Mesa Jous', precio: 91000, img:'imagenes/mesa/mesa11.png', categoria: 'Mesa' },
-    { id:036, producto: 'Puff Rey', precio: 35000, img:'imagenes/puff/puff1.png', categoria: 'Puff' },
-    { id:037, producto: 'Puff Tam', precio: 35000, img:'imagenes/puff/puff3.png', categoria: 'Puff' },
-    { id:040, producto: 'Puff Rous', precio: 45000, img:'imagenes/puff/puff4.png', categoria: 'Puff' },
-    { id:041, producto: 'Puff Guth', precio: 37000, img:'imagenes/puff/puff5.png', categoria: 'Puff' },
-    { id:042, producto: 'Puff Ray', precio: 38000, img:'imagenes/puff/puff6.png', categoria: 'Puff' },
-    { id:043, producto: 'Puff Fres', precio: 25000, img:'imagenes/puff/puff7.png', categoria: 'Puff' },
-    { id:044, producto: 'Puff Juol', precio: 35000, img:'imagenes/puff/puff8.png', categoria: 'Puff' },
+    { id:001, producto: 'Sillon Mand', precio: 80000, img:'imagenes/sillon/sillon.png', categoria: 'Sillon'},
+    { id:002, producto: 'Sillon Candy', precio: 60000, img:'imagenes/sillon/sillon2.png', categoria: 'Sillon'},
+    { id:003, producto: 'Sillon Matt', precio: 47000, img:'imagenes/sillon/sillon3.png', categoria: 'Sillon'},
+    { id:004, producto: 'Sillon Sindy', precio: 80000, img:'imagenes/sillon/sillon4.png', categoria: 'Sillon'},
+    { id:005, producto: 'Sillon Ted', precio: 90000, img:'imagenes/sillon/sillon5.png', categoria: 'Sillon'},
+    { id:006, producto: 'Sillon Thuk', precio: 70000, img:'imagenes/sillon/sillon6.png', categoria: 'Sillon'},
+    { id:007, producto: 'Sillon Grace', precio: 80000, img:'imagenes/sillon/sillon8.png', categoria: 'Sillon'},
+    { id:010, producto: 'Sillon Furt', precio: 90000, img:'imagenes/sillon/sillon9.png', categoria: 'Sillon'},
+    { id:011, producto: 'Sillon Huk', precio: 75000, img:'imagenes/sillon/sillon10.png', categoria: 'Sillon'},
+    { id:012, producto: 'Sillon Morf', precio: 87000, img:'imagenes/sillon/sillon11.png', categoria: 'Sillon'},
+    { id:013, producto: 'Sillon Mali', precio: 76000, img:'imagenes/sillon/sillon12.png', categoria: 'Sillon'},
+    { id:014, producto: 'Sillon Lou', precio: 80000, img:'imagenes/sillon/sillon15.png', categoria: 'Sillon'},
+    { id:015, producto: 'Silla Yug', precio: 79000, img:'imagenes/silla/silla.png', categoria: 'Silla'},
+    { id:016, producto: 'Silla Wind', precio: 64000, img:'imagenes/silla/silla1.png', categoria: 'Silla'},
+    { id:017, producto: 'Silla Meli', precio: 83000, img:'imagenes/silla/silla2.png', categoria: 'Silla'},
+    { id:020, producto: 'Silla Frut', precio: 35000, img:'imagenes/silla/silla3.png', categoria: 'Silla'},
+    { id:021, producto: 'Silla Xyl', precio: 47000, img:'imagenes/silla/silla4.png', categoria: 'Silla'},
+    { id:022, producto: 'Silla Grey', precio: 48000, img:'imagenes/silla/silla5.png', categoria: 'Silla'},
+    { id:023, producto: 'Mesa Sam', precio: 99000, img:'imagenes/mesa/mesa1.png', categoria: 'Mesa'},
+    { id:024, producto: 'Mesa Matt', precio: 89000, img:'imagenes/mesa/mesa2.png', categoria: 'Mesa'},
+    { id:025, producto: 'Mesa Saum', precio: 99000, img:'imagenes/mesa/mesa3.png', categoria: 'Mesa'},
+    { id:026, producto: 'Mesa Dot', precio: 89000, img:'imagenes/mesa/mesa4.png', categoria: 'Mesa'},
+    { id:027, producto: 'Mesa Frut', precio: 79000, img:'imagenes/mesa/mesa5.png', categoria: 'Mesa'},
+    { id:030, producto: 'Mesa Saim', precio: 69000, img:'imagenes/mesa/mesa6.png', categoria: 'Mesa'},
+    { id:031, producto: 'Mesa Ros', precio: 89000, img:'imagenes/mesa/mesa7.png', categoria: 'Mesa'},
+    { id:032, producto: 'Mesa Will', precio: 87000, img:'imagenes/mesa/mesa8.png', categoria: 'Mesa'},
+    { id:033, producto: 'Mesa Ann', precio: 95000, img:'imagenes/mesa/mesa9.png', categoria: 'Mesa'},
+    { id:034, producto: 'Mesa Drey', precio: 93000, img:'imagenes/mesa/mesa10.png', categoria: 'Mesa'},
+    { id:035, producto: 'Mesa Jous', precio: 91000, img:'imagenes/mesa/mesa11.png', categoria: 'Mesa'},
+    { id:036, producto: 'Puff Rey', precio: 35000, img:'imagenes/puff/puff1.png', categoria: 'Puff'},
+    { id:037, producto: 'Puff Tam', precio: 35000, img:'imagenes/puff/puff3.png', categoria: 'Puff'},
+    { id:040, producto: 'Puff Rous', precio: 45000, img:'imagenes/puff/puff4.png', categoria: 'Puff'},
+    { id:041, producto: 'Puff Guth', precio: 37000, img:'imagenes/puff/puff5.png', categoria: 'Puff'},
+    { id:042, producto: 'Puff Ray', precio: 38000, img:'imagenes/puff/puff6.png', categoria: 'Puff'},
+    { id:043, producto: 'Puff Fres', precio: 25000, img:'imagenes/puff/puff7.png', categoria: 'Puff'},
+    { id:044, producto: 'Puff Juol', precio: 35000, img:'imagenes/puff/puff8.png', categoria: 'Puff'},
     { id:045, producto: 'Comoda Rian', precio: 76000, img:'imagenes/comoda/comoda.png', categoria: 'Comoda'},
     { id:046, producto: 'Comoda Velt', precio: 86000, img:'imagenes/comoda/comoda1.png', categoria: 'Comoda'},
     { id:047, producto: 'Comoda Ram', precio: 66000, img:'imagenes/comoda/comoda2.png', categoria: 'Comoda'},
@@ -67,9 +84,11 @@ function carritoCard (){
             <th><img class="img-modal" src='${producto.img}'></th>            
             <th class="prod-modal text-center">${producto.producto}</th>
             <th class="precio-modal text-center"> $${producto.precio}</th>
-            <a class="boton-modal btn btn-white flex-end"><img src='imagenes/eliminar.png'></a>
+            
+            <a class="boton-modal btn style"><img src='imagenes/eliminar.png'></a>
         </tr>`
     })
+    
 }
 //-------------------------------------//
 //----------Creamos card--------------//
@@ -84,7 +103,7 @@ for (const producto of carrito){
     <div class="card-body p-4">
     <div class="producto text-center">
     <h3 class="fw-bolder">${producto.producto}</h3>    
-    <span>$${producto.precio}</span>    
+    <span>$${producto.precio}</span>     
     </div>
     </div>    
     <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
@@ -111,7 +130,8 @@ carrito.forEach((producto) =>{
         document.getElementById('card-compra').innerHTML = '';    
         
         
-    }
+    }    
+
 })
 
 //-------------------------------------//
@@ -119,7 +139,7 @@ carrito.forEach((producto) =>{
 //------------------------------------//
 //function eliminarProducto() {
 //    carrito.forEach((producto) => {
-//        const idButtonE = `borrar-card${producto.id}`;
+//        const idButtonE = `borrar-cart${producto.id}`;
 //        document.getElementById(idButtonE).onclick = () => {
 //            const eliminar = miCarrito.findIndex((acc, el) => acc + el.id === `${producto.id}`)
 //            if (index != -1) {
@@ -131,10 +151,34 @@ carrito.forEach((producto) =>{
 ////}
 
 // vaciar carrito
-//function limpiarCarrito() {
- //   document.getElementById('vaciar-carrito').innerHTML = "";
+//function vaciarCarrito() {
+//   document.getElementById('limpiarCarrito').innerHTML = "";
     
 //}
+//vaciarCarrito();
+
+//Revisa si un elemento ya existe en el carrito
+//miCarrito.forEach ((el) => {
+//    const existe = miCarrito.some((acc, el) => acc + el.id === `${producto.id}`);
+//    document.getElementById('card-compra').innerHTML = `${existe}`;
+///    if (existe) {
+//        //Actualizamos la cantidad
+//        const cantidad = miCarrito.map((el) => {
+//            if (el.id === `${producto.id}`) {
+//                el.cantidad++;
+//                return el; // retorna objeto actualizado
+///            } else {
+///                return el; // retorna los objetos que no son duplicados
+//           }
+//        });
+//        miCarrito = [...cantidad];
+//   } else {
+        //Agregar elementos al arreglo del carrito
+        miCarrito = [...miCarrito, el];
+//    }
+//    
+//})
+
 
 
 
